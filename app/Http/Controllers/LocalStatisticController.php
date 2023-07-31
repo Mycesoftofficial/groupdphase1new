3f23f40b0081690a289a8ec271c82c41faedecb9
Your Name <jxtsly@gmail.com>
2023-07-31 19:18:25 +0200

    insertion of data into the database

--- app/Http/Controllers/LocalStatisticController.php
+++ app/Http/Controllers/LocalStatisticController.php
@@ -15,10 +15,7 @@ class LocalStatisticController extends Controller
      */
     public function store(Request $request)
     {
-        // Validate the request data
-        // $request->validate([
-            // Add validation rules for each field if needed
-        // ]);
+    
             
         // Create a new LocalStatistic instance and fill it with the request data
         $localStatistic = new LocalStatistic($request->all());
