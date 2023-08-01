7cc23c2660a4f52d8e32000e3e2b5830c1b56111
KwasiEvans <evansow2002@gmail.com>
2023-07-31 16:43:49 -1200

    handled error

--- app/Http/Controllers/LocalStatisticController.php
+++ app/Http/Controllers/LocalStatisticController.php
@@ -5,14 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\LocalStatistic;
 
-class LocalStatisticController extends Controller
-{
-     /**
-     * Save the inputted data into the database.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+class LocalStatisticController extends Controller{
     public function store(Request $request)
     {
         // Validate the request data
@@ -20,11 +13,15 @@ class LocalStatisticController extends Controller
             // Add validation rules for each field if needed
         // ]);
             
-        // Create a new LocalStatistic instance and fill it with the request data
+        try {
+             // Create a new LocalStatistic instance and fill it with the request data
         $localStatistic = new LocalStatistic($request->all());
 
         // Save the new record to the database
         $localStatistic->save();
+        } catch (\Exception $e) {
+            return back()->with('error',$e->getMessage());
+        }
 
         // Redirect back to the input form with a success message
         return redirect()->back()->with('success', 'Data saved successfully!');
